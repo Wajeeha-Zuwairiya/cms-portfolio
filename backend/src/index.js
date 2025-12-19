8c013070c34acf5236e2d4a57601503a5ac25db4
Wajeeha-Zuwairiya <wzuwairiya@gmail.com>
2025-12-19 20:33:24 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,6 +27,7 @@ app.get('/', (req, res) => {
 const corsOptions = {
   origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
   optionsSuccessStatus: 200
 };
 
