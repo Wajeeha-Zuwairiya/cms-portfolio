856417d5e43ad0cab227e1e8de500bd440842f2b
Wajeeha-Zuwairiya <wzuwairiya@gmail.com>
2025-12-20 13:14:42 +0530

    Update blogRoutes.js

--- backend/src/routes/blogRoutes.js
+++ backend/src/routes/blogRoutes.js
@@ -2,16 +2,10 @@ const router = require("express").Router();
 const blogController = require("../controllers/blogController");
 const auth = require("../middleware/auth");
 
-// GET all blogs
 router.get("/", blogController.getBlogs);
-
-// CREATE blog with image
-router.post("/",auth, upload.single("image"), blogController.createBlog);
-
-// UPDATE blog with optional image
-router.put("/:id",auth, upload.single("image"), blogController.updateBlog);
-
-// DELETE blog
-router.delete("/:id",auth, blogController.deleteBlog);
+// REMOVED upload.single("image")
+router.post("/", auth, blogController.createBlog); 
+router.put("/:id", auth, blogController.updateBlog);
+router.delete("/:id", auth, blogController.deleteBlog);
 
 module.exports = router;
