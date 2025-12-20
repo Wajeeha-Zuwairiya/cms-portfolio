fc8e63ad003565554c07cd1be3e2f720d3475def
Wajeeha-Zuwairiya <wzuwairiya@gmail.com>
2025-12-20 13:31:24 +0530

    Update mediaRoutes.js

--- backend/src/routes/mediaRoutes.js
+++ backend/src/routes/mediaRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 
 router.get("/", getMedia);
-router.post("/", upload.single("file"), uploadMedia);
+router.post("/", mediaController.uploadMedia);
 router.delete("/:id", deleteMedia);
 
 module.exports = router;
